054e5aa0aec2d7c20e9b709778fa26242830c576
Harry Gamblin <harry.at.gamblin@gmail.com>
2023-11-26 19:56:42 +0000

    more gsheets work

--- python/gsheets_functions.py
+++ python/gsheets_functions.py
@@ -1,7 +1,16 @@
 import gsheets
 import pathlib
+import json
 from constants import SPREADSHEET_ID
+
+def _get_json_credentials(json_file):
+    with open(json_file, 'r') as output:
+        return json.load(output)
 
 cwd = pathlib.Path.cwd()
 
-print(cwd)
+credentials = _get_json_credentials(f"{cwd}/credentials.json")
+
+sheets = gsheets.Sheets(credentials=credentials)
+
+sheets.get(SPREADSHEET_ID)
