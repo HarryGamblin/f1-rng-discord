4dfe2f32663d8902315a606da4bc2c4091fdc502
Harry Gamblin <harry.at.gamblin@gmail.com>
2023-11-26 17:20:40 +0000

    testing info

---
+++ python/constants.py
@@ -0,0 +1,1 @@
+MESSAGE_PREFIX = "f1-rng "
--- python/main.py
+++ python/main.py
@@ -1,1 +1,17 @@
 import discord
+import gsheets_functions
+from constants import MESSAGE_PREFIX
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(MESSAGE_PREFIX):
+        pass
